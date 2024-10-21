a1729ce281e1a73b9396229f48dd58416ed546d7
Salma Barajas <salma.barajas.aldama@cbtis89.edu.mx>
2024-10-21 09:05:42 -0600

    Add files via upload

---
+++ P01Variables1.php
@@ -0,0 +1,24 @@
+<?php
+/* CBTIS 89
+   Programa que declara variables
+   Salma Lorena Barajas Aldama
+   3°A Programacion Matutino */
+   
+  $dia = 24; //Se declara una variable de tipo integer.
+  $sueldo = 758.43; //Se declara una variable de tipo double.
+  $nombre = " Ren Amamiya "; //Se declara una variable de tipo string.
+  $exite = true; //Se declara una variable boolean.
+
+  echo "Variable entera: ";
+  echo $dia;echo "<br>";
+  echo "Variable double: ";
+  echo $sueldo;echo "<br>";
+  echo "Variable string: ";
+  echo $nombre;echo "<br>";
+  echo "Variable boolean: ";
+  echo $exite;
+  echo "<br>";
+  $cadena1= "Hello";
+  $cadena2= "World";
+  echo $cadena1. " ".$cadena2;
+  ?>
