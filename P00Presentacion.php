4d49806664743bcccd406bd503bc6106107b4d84
Salma Barajas <salma.barajas.aldama@cbtis89.edu.mx>
2024-10-21 09:29:31 -0600

    Add files via upload

---
+++ P00Presentacion.php
@@ -0,0 +1,20 @@
+<?php
+/*
+/* CBTIS 89
+ Programa que imprime nuestros datos personales
+ Salma Lorena Barajas Aldama
+ 3°A Programacion Mat.*/
+
+echo "DATOS PERSONALES";
+echo "<br>";
+echo "Me llamo Salma Lorena Barajas Aldama";
+echo "<br>";
+echo "Tengo 16 años";
+echo "<br>";
+echo "Estudio en el CBTIS 89, en la carrera de programacion, tercer semestre, grupo A";
+echo "<br>","<br>";
+echo "DATOS GENERALES";
+echo "<br>";
+echo "Naci y vivo en la ciudad de Victoria de Durango";
+echo "<br>";
+echo "Estado civil: Con pareja";
